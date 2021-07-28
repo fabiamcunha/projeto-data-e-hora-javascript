6ecf9e1431226e868d52d6c31368e47e0440abfd
fabia cunha <fabiamcunha@gmail.com>
2021-07-28 15:46:44 -0300

    atualizar readme

--- projeto2/README.md
+++ projeto2/README.md
@@ -2,7 +2,12 @@
 
 ## Projeto relogio em javascript
 
-projeto desenvolvido com conhecementos adquiridos no curso online  b7web focado em trabalhar conhecimentos de data  e hora em javascript 
+projeto desenvolvido com conhecimentos adquiridos no curso online  b7web focado em trabalhar com a classe  **Date**  em javascript
+
+1  Primeiramente foi desenvolvido relogio analogico 
+2 uma relogio digital 
+3 data atual por extenso 
+4 alteração do layuot de acordo com a especificação da hoar do dia
 
 -HTML 
 -CSS
--- projeto2/script.js
+++ projeto2/script.js
@@ -5,43 +5,55 @@ let hElement=document.querySelector('.p_h');
 
 
 function updateClock(){
+
     const now=new Date();
     let hour =now.getHours();
     let minute=now.getMinutes();
     let second=now.getSeconds();
+    let day=now.getDate();
+    let month=now.getMonth();
+    let year=now.getUTCFullYear();
+    let semana=now.getDay()
+
+    var mesExtenso = ["Janeiro", "Fevereiro", "Março", 
+    "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", 
+    "Outubro", "Novembro", "Dezembro"];
+
+    let semanaExtenso=['domingo','segunda-feira','terça-feira','quarta-feira ','quinta-feira','sexta-feira','sabado'];
+    let data_atual=`${semanaExtenso[semana]},${fixZero(day)} de ${mesExtenso[month]} de ${year}`;
+
 
     digitalElement.innerHTML=`${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`;
 
     let sdeg=((360/60)*second)-90;
     let mdeg=((360/60) * minute)-90;
     let hdeg=((360/12) * hour)-90;
-
+    
     sElement.style.transform=`rotate(${sdeg}deg)`;
     mElement.style.transform=`rotate(${mdeg}deg)`;
     hElement.style.transform=`rotate(${hdeg}deg)`;
-      
+
+   
+  document.querySelector('.data').innerHTML=data_atual;
     if (6<=hour && hour <12) {
 
-        document.querySelector('#imagen').classList.add('manha');
+        document.querySelector('#back').classList.add('manha');
 
     }else if (hour < 18) {
 
-        document.querySelector('#imagen').classList.add('tarde');
+        document.querySelector('#back').classList.add('tarde');
 
     }else {
 
         document.querySelector('#imagen').classList.add('noite');
 
 }
-  
-   }
+           
+           
 
-  
-    
-   
-
+   }  
 function fixZero(time){
-    return time <10 ? `0${time}`:time
+    return time <10 ? `0${time}`:time;
 }
 
 
